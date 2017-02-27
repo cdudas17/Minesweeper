b667122b7a8f1cf6fdb4c942f8b1e0a38d21d46c
Matthew Shampine <shampinm@eos07.cis.gvsu.edu>
2017-02-26 19:47:06 -0500

    Adding functionality to the MineSweeperGame

--- src/MineSweeperGame.java
+++ src/MineSweeperGame.java
@@ -1,4 +1,26 @@
 
 public class MineSweeperGame {
 
+	private Cell[][] board;
+	
+	private GameStatus gameStatus; 
+	
+	private int row;
+	
+	private int col;
+	
+	public MineSweeperGame() {
+//		gameStatus = GameStatus.NotOverYet;
+		board = new Cell[row][col];
+	}
+	
+	public void createBoardDimensions(int size) {
+		try {
+			if (size < 2 || size > 24)
+				throw new NumberFormatException();
+		} catch (NumberFormatException e) {
+			
+		}
+		
+	}
 }
