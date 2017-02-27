8998c43be9f537ff0f33c0d355bcb010da12067f
shampinm <shampinm@eos07.cis.gvsu.edu>
2017-02-27 14:54:33 -0500

    Added the setMines() class that adds mines to the board.

--- src/MineSweeperPanel.java
+++ src/MineSweeperPanel.java
@@ -38,27 +38,6 @@ public class MineSweeperPanel extends JPanel implements ActionListener {
 		}
 	}
 
-	public void newGame(int size, int numMine) {
-		game = new MineSweeperGame(size, numMine);
-		setLength(game.getRow());
-
-		board = new JButton[length][length];
-		center.setLayout(new GridLayout(length, length));
-
-		for (int row = 0; row < length; row++)
-			for (int col = 0; col < length; col++) {
-				board[row][col] = new JButton("");
-				board[row][col].setPreferredSize(new Dimension(45, 45));
-				board[row][col].setBackground(Color.LIGHT_GRAY);
-				board[row][col].addActionListener(this);
-				center.add(board[row][col]);
-			}
-		center.revalidate();
-		center.repaint();
-
-		add(center, BorderLayout.CENTER);
-	}
-
 	public void setLength(int length) {
 		this.length = length;
 	}
