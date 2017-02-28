2c935574cac2a0e6b88932b22cf4c64ce0ba720d
Matthew Shampine <shampinm@mail.gvsu.edu>
2017-02-28 11:30:13 -0500

    Added a save/load feature. Just need to finish the zeroCell() method.

--- src/MineSweeperGame.java
+++ src/MineSweeperGame.java
@@ -1,28 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Random;
-
-//import javax.swing.JOptionPane;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class MineSweeperGame {
 
+	/** 2D array of cells that resembles a MineSweeper board */
 	private Cell[][] board;
 
+	/** holds the current status of the game */
 	private GameStatus gameStatus;
 
+	/** board row */
 	private int row;
 
+	/** board column */
 	private int col;
 
+	// TO-DO: maybe do not need this int, and method
 	private final int DEFAULT_SIZE = 8;
 
 	private final int DEFAULT_MINES = 10;
-
-	public MineSweeperGame() {
-		setGameStatus(GameStatus.NotOverYet);
-		createBoardDimensions(getDEFAULT_SIZE());
-		board = new Cell[row][col];
-		initialize();
-		setMines(DEFAULT_MINES);
-	}
 
 	public MineSweeperGame(int size, int numMines) {
 		setGameStatus(GameStatus.NotOverYet);
@@ -62,33 +63,42 @@ public class MineSweeperGame {
 		while (count < numMines) {
 			int r = randomLoc.nextInt(row);
 			int c = randomLoc.nextInt(col);
-			
+
 			if (!board[r][c].isMine()) {
 				board[r][c].setMine(true);
 				count++;
 			}
 		}
 	}
-	
+
 	public void selectCell(int row, int col) {
 		board[row][col].setExposed(true); // selects the cell the user clicked on
-		
+
 		if (board[row][col].isMine()) {
 			showMines(); // shows all of the mines when the user clicks on a mine
 			setGameStatus(GameStatus.Lost);
 		}
-		
+
 		if (board[row][col].getMineCount() == 0)
 			zeroCell(row, col);
-		
+
 		if (checkStatus())
 			setGameStatus(GameStatus.Won);
+		else
+			setGameStatus(GameStatus.NotOverYet);
 	}
-	
+
 	private boolean checkStatus() {
-		return false;
+		boolean status = true;
+		for (int row = 0; row < this.row; row++) {
+			for (int col = 0; col < this.col; col++) {
+				if (!board[row][col].isMine() && !board[row][col].isExposed())
+					status = false;
+			}
+		}
+		return status;
 	}
-	
+
 	private void showMines() {
 		for (int row = 0; row < this.row; row++) {
 			for (int col = 0; col < this.col; col++) {
@@ -98,18 +108,90 @@ public class MineSweeperGame {
 		}
 	}
 	
+	// TO-DO: finish when the user clicks on a zero spot
 	private void zeroCell(int row, int col) {
 
 	}
-	
+
 	public void mineCount() {
-		
+		for (int row = 0; row < this.row; row++) {
+			for (int col = 0; col < this.col; col++) {
+				if (board[row][col].isMine())
+					addMine(row, col);
+			}
+		}
 	}
-	
+
+	private void addMine(int row, int col) {
+		if (getCell(row - 1, col) != null)
+			board[row - 1][col].setMineCount(1);
+		if (getCell(row - 1, col + 1) != null)
+			board[row - 1][col + 1].setMineCount(1);
+		if (getCell(row, col + 1) != null)
+			board[row][col + 1].setMineCount(1);
+		if (getCell(row + 1, col + 1) != null)
+			board[row + 1][col + 1].setMineCount(1);
+		if (getCell(row + 1, col) != null)
+			board[row + 1][col].setMineCount(1);
+		if (getCell(row + 1, col - 1) != null)
+			board[row + 1][col - 1].setMineCount(1);
+		if (getCell(row, col - 1) != null)
+			board[row][col - 1].setMineCount(1);
+		if (getCell(row - 1, col - 1) != null)
+			board[row - 1][col - 1].setMineCount(1);
+	}
+
+	public void save(String filename) {
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(filename + ".txt");
+			writer.print(board[row][col].getMineCount() + ",");
+
+			for (int row = 0; row < this.row; row++) {
+				for (int col = 0; col < this.col; col++) {
+
+					if (board[row][col].isMine())
+						writer.print("t,");
+					else
+						writer.print("f,");
+
+					if (board[row][col].isFlagged())
+						writer.print("t,");
+					else
+						writer.print("f,");
+
+					if (board[row][col].isMine())
+						writer.println("t,");
+					else
+						writer.println("f,");
+				}
+			}
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Error: Unable to save game! ");
+		} finally {
+			writer.close();
+		}
+	}
+
+	public void load(String filename) {
+		Scanner fileReader = null;
+
+		try {
+			fileReader = new Scanner(new File(filename));
+
+			while (fileReader.hasNextLine()) {
+				// String input = fileReader.nextLine();
+			}
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(null, "Error: Unable to load game! ");
+		}
+	}
+
+	// TO-DO: maybe do not need this method
 	public Cell[][] getBoard() {
 		return board;
 	}
-	
+
 	public Cell getCell(int row, int col) {
 		return (row < 0 || col < 0 || row >= this.row || col >= this.col) ? null : board[row][col];
 	}
@@ -130,9 +212,11 @@ public class MineSweeperGame {
 		this.col = col;
 	}
 
+	// TO-DO: might not need these two methods
 	public int getDEFAULT_SIZE() {
 		return DEFAULT_SIZE;
 	}
+
 	public int getDEFAULT_MINES() {
 		return DEFAULT_MINES;
 	}
